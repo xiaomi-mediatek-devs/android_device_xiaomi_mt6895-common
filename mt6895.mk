12a3013359b44ec0cceee345348509133f92a202
danielml <daniel@danielml.dev>
2023-12-13 13:15:48 +0100

    mt6895-common: Introduce a wrapper for Xiaomi's vibratorfeature
    Change-Id: I24d5e17e839df1414a786a26045cda243880a7bc
    Signed-off-by: danielml <daniel@danielml.dev>

--- mt6895.mk
+++ mt6895.mk
@@ -304,13 +304,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
-$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
-
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vibratorfeature-wrapper
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
