08acaf9184f657653167504e9d5e1c5b85c142f1
Arian <arian.kulmer@web.de>
2024-03-14 13:46:02 +0100

    mt6895-common: udfps: Drop unnecessary setFingerDown on cancel
    HBM is usually not on when it gets cancelled
    
    Change-Id: I6c2c4836bf9dad627d4f97283b0fe8d2c398908a

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -209,7 +209,6 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
         LOG(INFO) << __func__;
         enrolling = false;
 
-        setFingerDown(false);
         setFodStatus(FOD_STATUS_OFF);
     }
 
