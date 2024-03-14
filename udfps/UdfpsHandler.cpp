d8c5ae85731a5c2e4caed7991e64ddb8217fb610
Arian <arian.kulmer@web.de>
2024-03-14 13:50:54 +0100

    mt6895-common: udfps: Add comments about setFingerDown usage
    Change-Id: Ifb0394a887cdabdad20208b5deb2386e7cc7f55a

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -178,18 +178,21 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
         LOG(INFO) << __func__;
+        // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(true);
     }
 
     void onFingerUp() {
         LOG(INFO) << __func__;
+        // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(false);
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
-            setFingerDown(false);
+           setFingerDown(false);
+
             if (!enrolling) {
                 setFodStatus(FOD_STATUS_OFF);
             }
