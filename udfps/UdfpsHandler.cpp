172fbfcd1abc1541c6dc832bf02cb37af3fc804f
Arian <arian.kulmer@web.de>
2024-03-14 18:49:30 +0100

    mt6895-common: udfps: Disable HBM directly on successfull authentication
    Change-Id: I336d4079cc41156135b5c68319c11d2ae640f548

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -222,7 +222,12 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
-           setFingerDown(false);
+            // Request to disable HBM already, even if the finger is still pressed
+            disp_local_hbm_req req;
+            req.base.flag = 0;
+            req.base.disp_id = MI_DISP_PRIMARY;
+            req.local_hbm_value = LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP;
+            ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
 
             if (!enrolling) {
                 setFodStatus(FOD_STATUS_OFF);
