3ad7436deb3b039e0b9c85bf21a51cd16f9c2e37
kxxt <rsworktech@outlook.com>
2024-03-15 08:13:52 +0800

    mt6895-common: udfps: Enable fod_status on finger down events instead for fpc_fod
    Unfortunately, the fpc_fod hwmodule does not send vendor messages
    when it starts to wait for fingerprints reliably...
    
    Change-Id: Ie66cbed024ce89092ecbb1bd9c965b35c59199f7

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -7,6 +7,7 @@
 
 #include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android-base/unique_fd.h>
 
 #include <poll.h>
@@ -96,6 +97,10 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
         mDevice = device;
         touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
         disp_fd_ = android::base::unique_fd(open(DISP_FEATURE_PATH, O_RDWR));
+
+        std::string fpVendor = android::base::GetProperty("persist.vendor.sys.fp.vendor", "none");
+        LOG(DEBUG) << __func__ << "fingerprint vendor is: " << fpVendor;
+        isFpcFod = fpVendor == "fpc_fod";
 
         // Thread to notify fingeprint hwmodule about fod presses
         std::thread([this]() {
@@ -209,6 +214,15 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
         lastPressX = x;
         lastPressY = y;
 
+        /*
+         * On fpc_fod devices, the waiting for finger message is not reliably sent...
+         * The finger down message is only reliably sent when the screen is turned off, so enable
+         * fod_status better late than never.
+         */
+        if (isFpcFod) {
+            setFodStatus(FOD_STATUS_ON);
+        }
+
         // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(true);
     }
@@ -234,12 +248,17 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
             }
         }
 
-        /* vendorCode
+        /* vendorCode for goodix_fod devices:
          * 21: waiting for finger
          * 22: finger down
          * 23: finger up
+         * On fpc_fod devices, the waiting for finger message is not reliably sent...
+         * The finger down message is only reliably sent when the screen is turned off, so enable
+         * fod_status better late than never.
          */
-        if (vendorCode == 21) {
+        if (!isFpcFod && vendorCode == 21) {
+            setFodStatus(FOD_STATUS_ON);
+        } else if (isFpcFod && vendorCode == 22) {
             setFodStatus(FOD_STATUS_ON);
         }
     }
@@ -272,6 +291,7 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
     fingerprint_device_t* mDevice;
     android::base::unique_fd touch_fd_;
     android::base::unique_fd disp_fd_;
+    bool isFpcFod;
     bool enrolling = false;
     uint32_t lastPressX, lastPressY;
 
