94ec62fb3ebe37259dc38bcde46f2c93385fd814
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:11:42 +0100

    mt6895-common: Set libperfmgr mode extension lib via soong config
    Change-Id: I39ac8867d98808832738f58eb6f50d456d6d34c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,9 +131,6 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 # Platform
 TARGET_BOARD_PLATFORM := mt6895
 
-# Power
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
-
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
--- mt6895.mk
+++ mt6895.mk
@@ -264,6 +264,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
+
 # Power-off Alarm
 PRODUCT_PACKAGES += \
     PowerOffAlarm
