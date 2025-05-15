a59104855a39b7b821cb47b6ee9ec075a3909c39
alextrack2013 <alextrack2013@gmail.com>
2025-05-15 10:23:53 +0200

    mt6895-common: udfps: `UdfpsHander` -> `UdfpsHandler`
    Change-Id: I43b799554304772f43500c7f83cfb7894ae9f9e4

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -72,7 +72,7 @@ static bool readBool(int fd) {
 
 }  // anonymous namespace
 
-class XiaomiMt6895UdfpsHander : public UdfpsHandler {
+class XiaomiMt6895UdfpsHandler : public UdfpsHandler {
   public:
     void init(fingerprint_device_t* device) {
         mDevice = device;
@@ -156,7 +156,7 @@ class XiaomiMt6895UdfpsHander : public UdfpsHandler {
 };
 
 static UdfpsHandler* create() {
-    return new XiaomiMt6895UdfpsHander();
+    return new XiaomiMt6895UdfpsHandler();
 }
 
 static void destroy(UdfpsHandler* handler) {
