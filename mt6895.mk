7a998e50fa4e8e191c04f682aa72a9ea0d1c6c00
ChrisCatto <chriscatto@proton.me>
2025-06-27 14:40:54 +0300

    mt6895-common: Drop `android.hardware.keystore.app_attest_key` feature
    * Since we added Keybox spoofing in our ROMs, generating attest keys
      has broke, hence we should not report support for it towards other
      apps, because it will simply not work.
    
    Change-Id: I6eaac45738d82ec3613273f3454a7f0000644c4a
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -223,7 +223,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.faketouch.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.faketouch.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
